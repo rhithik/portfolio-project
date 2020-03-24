abea7336716d43d48758b1947c3dcf254176d29f
Jeff <jeffmchaledesigns@gmail.com>
2020-03-24 17:32:48 -0400

    job app up and running with addition of postgresql as db

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodb',
+        'USER': 'postgres',
+        'PASSWORD': 'qawsedrfQAWSEDRF',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -122,4 +126,4 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-MEDIA_URL = '/coolstuff/'
+MEDIA_URL = '/media/'
